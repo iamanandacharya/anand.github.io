66684e3ad811ce803c627e1086adceb8ac887eb9
Anand Acharya <anandacharyadx3555@gmail.com>
2025-04-10 17:00:58 -0500

    update code

--- src/app/Pages/components/education/experiencesEn.ts
+++ src/app/Pages/components/education/experiencesEn.ts
@@ -39,6 +39,15 @@ export const certification = [
         detailIsDisplayed: false,
     },
     {
+        title: "React Native - The Practical Guide [2024]",
+        company: 'Udemy',
+        period: "",
+        technologies: "https://www.udemy.com/certificate/UC-e3c31679-948b-4b7e-bdc3-dda6eb9862ab/",
+        missions: [
+            ],
+        detailIsDisplayed: false,
+    },
+    {
         title: "Angular - The Complete Guide",
         company: 'Udemy',
         period: "",
@@ -55,6 +64,15 @@ export const certification = [
         missions: [
             ],
         detailIsDisplayed: false,
+    },
+    {
+        title: "Generative AI Essentials ",
+        company: 'Amazon Web Service(AWS)',
+        period: "",
+        technologies: "https://www.credly.com/org/amazon-web-services/badge/aws-partner-generative-ai-essentials",
+        missions: [
+            ],
+        detailIsDisplayed: false,
     }
 
     
@@ -66,14 +84,12 @@ export const experiencesEn2 = [
        period: "Aug 2023 - Current",
        technologies: "Ionic Framework, angular, Single-SPA Angular, Primeng, Typescript, JIRA, JavaScript, NodeJS, SCSS, CSS, Cordova, Capacitor, Java Spring, GitHub, AWS Services, VS Code, XCODE, Android Studio, Postman, Chrom/Safari Dev Tools, SourceTree, MicroService, ExpressJS, MySql, Postman.",
        missions: [
-           "• Lead full-stack development of enterprise-scale web applications and design scalable microservices using Node.js, Angular 17, TypeScript, Express.js, and AWS Lambda, with infrastructure provisioned via CloudFormation. Implement REST APIs, SFG for seamless data sharing, integrate Kafka for event-driven messaging, manage data with PostgreSQL, and use SQS/SNS for queue management and notifications. Enhance CI/CD pipelines with GitHub Actions, Jenkins, Sonarube, ensure security with IAM Roles/Policies, monitor performance with AWS CloudWatch Logs, and validate functionality using Jest, and Mocha. Develop responsive UI components with Angular 17 and PrimeNG, reducing deployment times by 50%.",
-           "• Develop 4 web applications and 2 cross-platform mobile apps (Android, iOS) using Ionic 7, Angular 17, TypeScript, and AWS Serverless architecture, improving user engagement by 25%.",
-           "• Integrate advanced micro-frontend architectures, enhancing modularity and scalability across web applications, resulting in 30% faster frontend load times.",
-           "• Optimize system performance and scalability by configuring databases, managing AWS Parameter Store, and deploying containerized applications on ECS. Utilize advanced Node.js libraries, async programming, and robust API designs to ensure secure, efficient, and reliable data transmission.",
-           "• Develop and optimize feature-rich frontend mobile and web applications by implementing advanced Angular functionalities, including custom Directives/Pipes, Multi-Language Support, Reactive Forms with validation, Auth-Guards, RxJS Observables, NgRx, and HTTP Interceptors. Integrate Cordova/Capacitor plugins for cross-platform compatibility and enhance UI/UX with responsive design using CSS/SCSS, Angular Material, PrimeNG, and Bootstrap. Deploy apps to the App Store and Play Store, reducing production build size and improving performance. Implement features such as QR/Barcode scanning, file uploads/downloads, i18n, JWT-based authentication, MSAL authorization, and ensure code quality using Jasmine/Karma unit testing and GitHub Actions for CI/CD.",
-           "• Collaborate with cross-functional teams, drive project delivery using Agile methodologies, and optimize frontend and backend performance by 20%.",
-           "• Collaborate with stakeholders to analyze requirements through direct communication, implement flowchart designs, create documentation for current data structures and technology stacks, and translate requirements into the development phase."
-       ],
+           "• Lead full-stack development of enterprise-scale web and mobile applications, design scalable microservices using Node.js, Angular, Ionic, TypeScript, Cordova, capacitor, Express.js, Java, and AWS Lambda. Analyze and collaborate on technical requirements with product managers, backend engineers, and designers, drive project delivery using Agile methodologies, and optimize frontend and backend performance by 20% within 2 quarters.",
+           "• Developing and architecting feature-rich web and hybrid mobile apps with advanced Angular and Ionic functionalities. Leading the migration from Cordova to Capacitor for enhanced performance, taking ownership of the entire SDLC cycle, integrating Cordova/Capacitor plugins for cross-platform compatibility, responsive design, and deploying apps to the App Store and Play Store. Following Scrum and Agile practices, enhancing UI/UX with CSS/SCSS, Ionic UI Material, PrimeNG, and Bootstrap.",
+           "• Engineer and maintain 4 high-performing single-page web applications and 2 cross-platform mobile apps using Ionic 7 and Angular 17, increasing user engagement by 25% within 3 months. Design and manage scalable microfrontend architecture for efficient code reuse, optimize CI/CD workflows with Terraform, Jenkins, and Gradle, reducing deployment times by 50%. Implement MSAL(follow OIDC) and OAuth token-based security with Identity and Access Management for authentication and authorization, and conduct unit, system, and E2E testing using Mocha, Jest, and JUnit to ensure application reliability, providing guidance to the QA team.",
+           "• Optimizing API documentation with Swagger, monitoring data operations using AWS CloudWatch, and handling data efficiently with PostgreSQL. Improving messaging systems with SFG, SQS/SNS, and Kafka, while strengthening security through IAM roles and policies. Automating AWS infrastructure provisioning with Terraform to reduce manual effort. Developing and validating high-performance CRUD APIs with Node.js, utilizing Chai, Jest, and Mocha, which has led to a 50% reduction in deployment times. Collaborating effectively with onsite and offshore teams, and external stakeholders to drive seamless project execution.",
+           "• Architected and containerized middleware microservices using Docker, Terraform, and ECS; managed PostgreSQL on AWS Aurora; optimized CI/CD pipelines with Jenkins and GitHub Actions. Designed IaC with CloudFormation and implemented event-driven Lambda functions for scalable, serverless data processing.",
+        ],
        detailIsDisplayed: false,
    },
    {
@@ -82,31 +98,43 @@ export const experiencesEn2 = [
        period: "OCT 2022 - May 2023",
        technologies: "Angular, Ionic, SCSS, Discord-APIs, Typescript, HTML5/CSS, Plivo, Twilio , GitHub ACTION, DevOps, Bootstrap, Postman, JSON, Ionic-App Flow, NodeJS, Capacitor, Onesignal Push Notification, VS Code, MySQL, AWS Services, MySQL, Xcode, Android Studio, Chrome/Safari Dev Tools, Postman.",
        missions: [
-           "• Engineered a scalable AI-powered B2B SaaS platform for global communication, maintaining two cross-platform mobile and web applications with Angular, Ionic, Typescript and Node.js.",
-           "• Implemented RESTful APIs, custom directives, and advanced UI features, reducing response time by 15% and enhancing the overall user experience.",
-           "• Implemented various Angular (Single Page Application) features such as Directives, Decorators, DI, Rest API Services, Routing, Pipes, AOT, JIT, Lazy Loading, RX.JS, NgRx, Authentication and Authorization using Firebase  and Auth-Guards, Animation, Angular Material, Unit Test, Angular Animation, Rest Api, Http-Interceptor and fetching JWT Tokens, Reactive forms and validation, Bootstrap.",
+           "•Led the development of a scalable, AI-powered B2B SaaS platform for global communication, architecting and delivering hybrid mobile and web applications using Angular, Ionic, React Native, HTML/SCSS, TypeScript, Capacitor, and Node.js. Mentored junior developers, coordinated with cross-functional teams, and drove feature delivery from design to deployment.",
+           "• Built and published 2 mobile/web apps with Ionic, rendered to the Cordova and Capacitor platforms, integrating VoIP services (Twilio/Plivo), WebRTC based streaming, and OneSignal push notifications using Capacitor (Android & iOS Swift) for seamless real-time communication, responsive UI for Android/iOS and CI/CD deployment process in dev, QA, and prod.",
            "• Developed, Debugged, maintained, and published 2 mobile and web applications using Ionic, Angular, Typescript, SCSS, HTML5/CSS, NodeJS, PhoneGap, Cordova, and Capacitor.",
-           "• Integrated VoIP updates, live chat, and real-time data with Twilio and Plivo, improving real-time customer communication by 40%."
+           "• Engineered Android/iOS communication features and integrated with RESTful APIs Design, advanced Angular functionalities (NgRx, JWT, lazy loading), designed responsive mobile interfaces, coordinated and mentored with internal and external members, enhanced real-time customer interaction by 40% within 2 months, and application performance by 15%."
        ],
        detailIsDisplayed: false,
    },
    {
+    title: "Senior IT Consultant",
+    company: 'ITC Infotech, Client: BATA',
+    period: "Feb 2022 - Oct 2022 ",
+    technologies: "Ionic, angular, SCSS, Capacitor, Cordova, ElectronJS, Filters, Services, Mobile & Web app, JavaScript,  Typescript, OneSignal, AWS LightSail, Bootstrap, HTML5/CSS, GitHub, Postman, US Client, JSON, Rest APIs, VS Code, NgRx, Xcode, Android Studio, Chrome/Safari Dev Tools, Firebase, SIT, UIT, ERP, CRM, Live AI Tools Chat, B2B, B2C, Publish App, Agile, Cypress, Calling-Service, Lead Project, Location tracking, Full-Stack Development.",
+    missions: [
+    "• Delivered a high-standard CRM solution, leveraging Angular, Ionic, Typescript, HTML/SCSS, and NodeJS to create a unified platform that revolutionized global operations for 100,000+ users, expertly integrating complex financial and ERP systems while ensuring exceptional performance and user experience.",
+    "• Prioritized and translated business requirements into functional ZPL updates, ensuring accuracy and alignment. Led GDPR- compliant data management and Agile projects, conducted code reviews, and mentored juniors. Conducted thorough testing with teams and stakeholders, managed SIT and UAT phases, and facilitated successful deployment, improving security and compliance by 20% within 1 quarter while adhering to ISO 9001 and CMMI.",
+    "• Pioneered the integration of Bluetooth printing and ZPL formatting, architected robust REST APIs and cloud-based data solutions across 18 countries, and optimized Angular & Ionic applications with advanced internationalization and rigorous testing, driving significant operational efficiency and global data integrity."  
+    ],
+    detailIsDisplayed: false,
+},
+   {
        title: "Senior Mobile, Web and Desktop Application Engineer",
-       company: 'Wing AI Technology Inc, Berkeley, CA',
+       company: 'Wing Assistant',
        period: "June 2020  - November 2021 ",
        technologies: "Ionic, angular, SCSS, Capacitor, Cordova, ElectronJS, Filters, Services, Mobile & Web app, JavaScript,  Typescript, OneSignal, AWS LightSail, Bootstrap, HTML5/CSS, GitHub, Postman, US Client, JSON, Rest APIs, VS Code, NgRx, Xcode, Android Studio, Chrome/Safari Dev Tools, Firebase, SIT, UIT, ERP, CRM, Live AI Tools Chat, B2B, B2C, Publish App, Agile, Cypress, Calling-Service, Lead Project, Location tracking, Full-Stack Development.",
        missions: [
-       "• Designed and developed AI-powered B2B/B2C mobile, desktop, and web applications using Angular, Ionic, ElectronJS, GCP and Node JS, PHP increasing company revenue by 20% through successful app launches.",
+       "• Led the design and development of AI-driven B2B/B2C mobile, desktop, and web applications using Angular, Ionic, Cordova, Capacitor, TypeScript/JavaScript, HTML/SCSS, React Native, MongoDB, ElectronJS, GCP, ApigeeX, and Node.js. Created roadmaps for application architecture and enhanced technical vision, boosting company revenue by 20% through successful launches and cross-functional collaboration.",
        "• Gained strong leadership skills by managing projects from start to finish individual and collaborate with testing team, backend team, and Business Team.",
        "• Implemented various functionality for Web and Mobile Application such as live AI ChatBoot, Push-notification, responsive UI, Angular Features, Stripe, Apple and G-Pay Payment ingratiation, authentication, AWS service, RXJS, Location Tracking using Google map and geocoding, State Management using NgRx, Unit test using Jest and Mocha, speech Recognition, Event Organization, Rest APIs, Google-Firebase, Ts-lint Pattern, Text-To-Speech, Analytic Data using Google-Analytic, 2-Factor Authentication and Migrating application from Angular 5 to 12 and Ionic 3 to 5.",
-       "• Exhibited strong technical aptitude and application expertise resulting in optimized performance, continuous improvement recommendations and product innovation."
+       "• Exhibited strong technical aptitude and application expertise resulting in optimized performance, continuous improvement recommendations and product innovation.",
+       "Developed a live AI chat module, push notifications, Tinder-style UI, payment integration, authentication, and OneSignal. Built Hybrid Mobile and web apps using AWS, Google Maps, NgRx for state management and tracking geolocation. Engaging with internal and external developers to integrate speech recognition, calendars, Authorization, and authentication using 2-Fctor and OIDC, REST APIs, Firebase, and offline data handling; automated CI/CD with GitHub Actions; Ionic App Flow; and YML-driven build pipelines."
            ],
        detailIsDisplayed: false,
    },
    {
        title: "Senior Angular and Ionic App Developer",
-       company: "IBRAINERS LTD",
-       period: "2016 - 2017",
+       company: "Freelancer, Client- IBRAINERS LTD",
+       period: "Jul 2019 - Jun 2020",
        technologies: "Ionic, angular, Capacitor, HTML5/CSS, GitHub, Typescript, JavaScript, B2B, Postman, JSON, Rest APIs, VS Code, SCSS, NgRx, Xcode, Android Studio, Chrome/Safari Dev Tools, B2C, Firebase, Karma.",
        missions: [
            "• Developed and maintained web and mobile applications focused on education and news using Angular, TypeScript, Ionic Framework, and Node.js, improving content delivery efficiency by 15%.",
@@ -154,7 +182,7 @@ export const experiencesEn2 = [
    {
        title: "Java Developer",
        company: 'Maxgen Technologies, Ahmedabad, India',
-       period: "2016 - 2017",
+       period: "Sep 2016 - Feb 2017",
        technologies: 'JAVA 8, J2EE, JSP, Serverlet, Javascript, Ajax, MySql',
        missions: [
            "Implemented ERP System and Employee Payroll System Module, which manage employee profile, salary, allowances, and deduction information. Developed full-stack J2EE apps with Java, JSP, JavaScript, Ajax on the frontend, and Servlet, EJB, Hibernate on the backend, integrating MongoDB and MySQL databases.",
